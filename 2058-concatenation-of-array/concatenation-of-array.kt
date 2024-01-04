00d68ae48cff9fac1d623ada3fa2fc65180e63a2
Gideon Rotich <giddyrotich433@gmail.com>
2024-01-04 08:53:22 +0300

    Delete 2058-concatenation-of-array directory

--- 2058-concatenation-of-array/concatenation-of-array.kt
+++
@@ -1,9 +0,0 @@
-class Solution {
-    fun getConcatenation(nums: IntArray): IntArray {
-        
-        //using the spread operator
-        val array = intArrayOf(*nums,*nums)
-
-        return array
-    }
-}
