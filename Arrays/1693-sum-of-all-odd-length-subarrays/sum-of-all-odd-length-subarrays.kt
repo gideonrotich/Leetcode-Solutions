352bc986e5ae9b52984860da01c6813a4a6d5f04
Gideon Rotich <giddyrotich433@gmail.com>
2024-01-08 08:39:35 +0300

    Time: 196 ms (21.74%) | Memory: 34.8 MB (43.48%) - LeetSync

--- Arrays/1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.kt
+++ Arrays/1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.kt
@@ -1,12 +1,17 @@
 class Solution {
     fun sumOddLengthSubarrays(arr: IntArray): Int {
-         var oddSum = 0
-    
-    for (i in 1..arr.size step 2) {
-        oddSum += arr.toList().windowed(i).map { it.sum() }.sum()
-    }
-    
-    return oddSum
+      
+      var count = 0
+
+      for(i in arr.indices){
+          for (k in i until arr.size step 2){
+              for(j in i..k){
+                  count += arr[j]
+              }
+          }
+      }
+
+      return count
 
 
     }
