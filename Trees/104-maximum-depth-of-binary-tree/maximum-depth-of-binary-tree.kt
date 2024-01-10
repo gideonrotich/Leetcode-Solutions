be90528a9ebf367caf4c5d59e2f3605e709eee1b
Gideon Rotich <giddyrotich433@gmail.com>
2024-01-10 21:35:28 +0300

    Time: 189 ms (25.65%) | Memory: 37 MB (73.40%) - LeetSync

---
+++ Trees/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.kt
@@ -0,0 +1,19 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun maxDepth(root: TreeNode?): Int {
+       if(root == null){
+           return 0
+       }
+
+       return maxOf(maxDepth(root.left),maxDepth(root.right)) + 1
+    }
+}
