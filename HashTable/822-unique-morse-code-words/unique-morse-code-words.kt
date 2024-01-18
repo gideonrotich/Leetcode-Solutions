9a9732ebe52fc33034f27811d9eef9d11a243a43
Gideon Rotich <giddyrotich433@gmail.com>
2024-01-18 09:11:18 +0300

    Time: 178 ms (58.82%) | Memory: 35.9 MB (52.94%) - LeetSync

--- HashTable/822-unique-morse-code-words/unique-morse-code-words.kt
+++ HashTable/822-unique-morse-code-words/unique-morse-code-words.kt
@@ -6,18 +6,37 @@ class Solution {
             ".--", "-..-", "-.--", "--.."
         )
 
+        val alphabet = "abcdefghijklmnopqrstuvwxyz"
+
         val transformations = HashSet<String>()
 
         for (word in words) {
             val transformation = StringBuilder()
 
             for (char in word) {
-                transformation.append(morseCodes[char - 'a'])
+                transformation.append(morseCodes[alphabet.indexOf(char)])
             }
 
             transformations.add(transformation.toString())
         }
 
         return transformations.size
+
+        //solution 2
+        // val alphabet = "abcdefghijklmnopqrstuvwxyz";
+        // val morseCodeAlphabet = arrayOf(".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..")
+        // val morseWords = mutableSetOf<String>()
+
+        // for (word in words) {
+        //     var wordString = ""
+
+        //     for (letter in word) {
+        //         wordString += morseCodeAlphabet[alphabet.indexOf(letter)]
+        //     }
+
+        //     morseWords.add(wordString)
+        // }
+
+        // return morseWords.size
     }
 }
