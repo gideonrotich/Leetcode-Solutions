0a0b69c98862b172b81149726a5d27e27d7dc6c7
Gideon Rotich <giddyrotich433@gmail.com>
2024-01-26 21:52:58 +0300

    Time: 153 ms (18.06%) | Memory: 33.2 MB (45.48%) - LeetSync

--- DynamicProgramming/1013-fibonacci-number/fibonacci-number.kt
+++ DynamicProgramming/1013-fibonacci-number/fibonacci-number.kt
@@ -1,20 +1,36 @@
 class Solution {
+    val cache = HashMap<Int,Int>()
+
     fun fib(n: Int): Int {
-         val dp = IntArray(n+1)
+        //using dynamic programming
+        //  val dp = IntArray(n+1)
 
-         if(n==0){
-             return 0
-         }
+        //  if(n==0){
+        //      return 0
+        //  }
 
-         //base cases
+        //  //base cases
 
-         dp[0] = 0
-         dp[1] = 1
+        //  dp[0] = 0
+        //  dp[1] = 1
 
-         for(i in 2..n){
-             dp[i] = dp[i-1] + dp[i-2] 
-         }
+        //  for(i in 2..n){
+        //      dp[i] = dp[i-1] + dp[i-2] 
+        //  }
 
-         return dp[n]
+        //  return dp[n]
+
+        //using memoization
+
+        if(n <= 1){
+            return n
+        }
+
+        if(!cache.containsKey(n)){
+            cache[n] = fib(n-1)+fib(n-2)
+        }
+
+        return cache[n]!!
+
     }
 }
