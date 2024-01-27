8386586f72c0a56d594abb4ee8ca866d1d436cc4
Gideon Rotich <giddyrotich433@gmail.com>
2024-01-27 11:07:23 +0300

    Time: 141 ms (60.86%) | Memory: 33.6 MB (84.05%) - LeetSync

--- DynamicProgramming/198-house-robber/house-robber.kt
+++ DynamicProgramming/198-house-robber/house-robber.kt
@@ -1,21 +1,21 @@
 class Solution {
     fun rob(nums: IntArray): Int {
-          val n = nums.size
+       val n = nums.size
 
-        if (n == 0) return 0
+       val dp = IntArray(n)
+
+         if (n == 0) return 0
         if (n == 1) return nums[0]
 
-        val dp = IntArray(n)
+       //base cases
+       dp[0] =  nums[0]
+       dp[1] = maxOf(nums[0],nums[1])
 
-        // Base cases
-        dp[0] = nums[0]
-        dp[1] = maxOf(nums[0], nums[1])
+       for(i in 2 until n){
+           dp[i] = maxOf(dp[i-2]+nums[i],dp[i-1])
+       }
 
-        for (i in 2 until n) {
-            // Choose the maximum between robbing the current house and skipping it
-            dp[i] = maxOf(dp[i - 2] + nums[i], dp[i - 1])
-        }
+       return dp[n-1]
 
-        return dp[n - 1]
     }
 }
