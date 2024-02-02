8f368836034135ecd07797c9e4b922912d61fb54
Gideon Rotich <giddyrotich433@gmail.com>
2024-02-02 11:05:56 +0300

    Time: 4210 ms (5.01%) | Memory: 51.5 MB (37.53%) - LeetSync

--- Arrays/189-rotate-array/rotate-array.kt
+++ Arrays/189-rotate-array/rotate-array.kt
@@ -1,13 +1,14 @@
 class Solution {
     fun rotate(nums: IntArray, k: Int): Unit {
+      
         var count = 0
+
         while(count < k){
             val test = nums.sliceArray(nums.size-1 until nums.size) + nums.sliceArray(0 until nums.size-1)
 
             for(i in nums.indices){
                 nums[i] = test[i]
             }
-
             count++
         }
     }
